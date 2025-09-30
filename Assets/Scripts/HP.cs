6f5d9800301aacc740aae2121c4e2bd8e306feea
Xycifer <zach020506@gmail.com>
2025-09-30 08:52:56 +0200

    HP & hslider

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HP : MonoBehaviour
 {
+    public int health;
+    public int maxHealth = 1;
+    public Slider hslider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        health = maxHealth;
+        hslider.maxValue = maxHealth;
+        hslider.value = health;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
