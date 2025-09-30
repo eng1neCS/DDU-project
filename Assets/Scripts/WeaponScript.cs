14010548ab3f75dc02c3940d9e52f6afaa8200df
Xycifer <zach020506@gmail.com>
2025-09-30 10:42:09 +0200

    Guns

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -1,16 +1,40 @@
+using UnityEngine.Events;
 using UnityEngine;
 
 public class WeaponScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public UnityEvent OnGunShoot;
+    public float GunCooldown;
+
+    public bool Automatic;
+
+    private float CurrentCooldown;
     void Start()
     {
-        
+        CurrentCooldown = GunCooldown;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Automatic)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                OnGunShoot?.Invoke();
+                CurrentCooldown = GunCooldown;
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (CurrentCooldown <= 0f)
+                {
+                    OnGunShoot?.Invoke();
+                    CurrentCooldown = GunCooldown;
+                }
+            }
+        }
+        CurrentCooldown -= Time.deltaTime;
     }
 }
